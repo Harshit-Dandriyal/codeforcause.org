d56179271a6eda21084bf74370377374e7860908
Harshit-Dandriyal <dandriyalh9318@gmail.com>
2020-09-27 17:59:21 +0530

    Revert "Merge pull request #1 from codeforcauseorg/development"
    This reverts commit 2c6b8dcb9ffa8ccfcf26a9783250b6c4fc6dc287, reversing
    changes made to da999a6d11045281c6163a507b50aba93dc76e94.

--- src/views/pages/CLView/Hero.js
+++ src/views/pages/CLView/Hero.js
@@ -1,17 +1,68 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import clsx from 'clsx';
+
+import { Box, Container, Typography, makeStyles } from '@material-ui/core';
 import ApplyNowModal from './ApplyNowModal';
-import HeroWithOneButton from '../../../components/Hero/HeroWithOneButton';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    minHeight: '350px',
+    color: '#FFF',
+    padding: '100px 70px',
+    [theme.breakpoints.down('md')]: {
+      paddingLeft: 15,
+      paddingRight: 15
+    }
+  },
+  main: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: '0px',
+    color: '#FFF'
+  },
+  btn: {
+    backgroundColor: '#A60000',
+    color: '#ffffff',
+    textTransform: 'capitalize',
+    [theme.breakpoints.down('sm')]: {
+      width: '100%'
+    },
+    '&:hover': {
+      backgroundColor: 'rgba(166, 0, 0, 0.8)'
+    }
+  }
+}));
 
 function Hero({ className, ...rest }) {
+  const classes = useStyles();
+
   return (
-    <HeroWithOneButton
-      title="Campus Leader"
-      subtitle="Stand Up and fight for the Cause with the Code For Cause"
-      className
-      backgroundImage="/static/campusLeaders/hero.png"
-      component={<ApplyNowModal />}
-    />
+    <div
+      className={clsx(classes.root, className)}
+      style={{
+        backgroundImage: 'url("/static/campusLeaders/hero.png")',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat'
+      }}
+      {...rest}
+    >
+      <Container maxWidth="lg">
+        <div className={classes.main}>
+          <Typography variant="h1">Campus Leaders</Typography>
+          <Box mt={2}>
+            <Typography variant="body1" align="center">
+              Stand Up and fight for the Cause with the Code For Cause
+            </Typography>
+          </Box>
+          <Box mt={2}>
+            <ApplyNowModal />
+          </Box>
+        </div>
+      </Container>
+    </div>
   );
 }
 
